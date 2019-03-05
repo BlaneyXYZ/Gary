aff1962fef4ea7374780261a5b8aff051d9dcff3
Nathan Blaney <nathan@blaney.xyz>
2019-03-04 23:55:02 -0500

    changes?

--- gary/plugins/porny.py
+++ gary/plugins/porny.py
@@ -5,18 +5,19 @@ from gary.util import http
 def porny_random(message):
     """porny - returns a random porny comment"""
 
-    request = http.get_json("http://alfred.blny.me:5000/api/random")
+    request = http.get_json("http://alfred.cloud.blny.me:5000/api/random")
     id = request["id"]
     comment = request["comment"]
-    message.reply(comment)
+    message.reply("{} (ID:{})".format(comment, id))
 
 @respond_to('porny (.*)')
 def porny_comment(message, text):
     """porny (id) - returns a specific porny comment"""
     if text.isdigit():
-        request = http.get_json("http://alfred.blny.me:5000/api/comment/{}".format(text.replace(" ", "+")))
+        request = http.get_json("http://alfred.cloud.blny.me:5000/api/comment/{}".format(text.replace(" ", "+")))
         id = request["id"]
+        source = request["source"]
         comment = request["comment"]
-        message.reply(comment)
+        message.reply("{} - {}".format(comment,source))
     else:
         message.reply("Please enter a valid comment ID")
